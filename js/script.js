dd42eb3ee9c5955292435a298cd91e266bad9ccd
sreejith-ms <sreejith.ms@zyxware.com>
2017-03-16 13:24:43 +0530

    fade effect changed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   $('#test').audThumb({
     margin: 10,
     transitionType: 'fullTransition', // singleTransition/fullTransition
-    transitionMod: 'slide',  // slide/fade
+    transitionMod: 'fade',  // slide/fade
     responsive: {
         0: {
           items: 2,
